80eb38d8bcac12572803b8648afc0d87d835b2e7
nganvn <nganvn2110@gmail.com>
2020-06-20 21:49:07 +0700

    reconfig database - revoke relationships

--- api/src/main/kotlin/com/matb/ordering/api/Controllers/VendorController.kt
+++ api/src/main/kotlin/com/matb/ordering/api/Controllers/VendorController.kt
@@ -5,6 +5,8 @@ import com.matb.ordering.api.models.entities.Food
 import com.matb.ordering.api.models.entities.Vendor
 import com.matb.ordering.api.models.repositories.FoodRepository
 import com.matb.ordering.api.models.repositories.VendorRepository
+import com.matb.ordering.api.models.responses.VendorResponse
+import com.matb.ordering.api.models.toVendorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -19,19 +21,34 @@ class VendorController(private val vendorRepository: VendorRepository, private v
     }
 
     @GetMapping("/vendor/{id}")
-    fun getVendorById(@PathVariable(value = "id") vendorId: Int): ResponseEntity<Vendor>{
-        return ResponseEntity(vendorRepository.findAllById(vendorId),  HttpStatus.OK)
+    fun getVendorById(@PathVariable(value = "id") vendorId: Int): ResponseEntity<VendorResponse>{
+        var vendor = vendorRepository.findAllById(vendorId)
+        return ResponseEntity(
+                vendor.toVendorResponse(
+                        foodRepository.findAllByVendorId(vendor.id!!)
+                ),
+                HttpStatus.OK)
     }
 
     @PostMapping("/vendor/{id}")
-    fun createFood(@PathVariable (value = "id") vendorID: Int, @RequestBody food: Food): ResponseEntity<Food> {
-        var vendor = vendorRepository.findAllById(vendorID)
-        food.vendor = vendor
+    fun createFood(@PathVariable (value = "id") vendorId: Int, @RequestBody food: Food): ResponseEntity<Food> {
+        food.vendorId = vendorId
         return ResponseEntity(foodRepository.save(food), HttpStatus.OK)
     }
-
     @GetMapping("/vendor")
     fun getAllVendor(): ResponseEntity<List<Vendor>>{
-        return ResponseEntity(vendorRepository.findAll(), HttpStatus.OK)
+        return ResponseEntity(
+                vendorRepository.findAll(),
+                HttpStatus.OK
+        )
     }
+/*    @GetMapping("/vendor")
+    fun getAllVendor(): ResponseEntity<List<VendorResponse>>{
+        return ResponseEntity(
+                vendorRepository.findAll().map {
+                    it.toVendorResponse(foodRepository.findAllByVendorId(it.id!!))
+                },
+                HttpStatus.OK
+        )
+    }*/
 }

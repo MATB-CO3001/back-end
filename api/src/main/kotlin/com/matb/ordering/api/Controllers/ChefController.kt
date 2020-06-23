30e64b4c2ef75848be49e08e6981384128a53f8f
nganvn <nganvn2110@gmail.com>
2020-06-23 20:14:28 +0700

    add ChefController

--- api/src/main/kotlin/com/matb/ordering/api/Controllers/ChefController.kt
+++ api/src/main/kotlin/com/matb/ordering/api/Controllers/ChefController.kt
@@ -1,4 +1,21 @@
 package com.matb.ordering.api.Controllers
 
-class ChefController {
+import com.matb.ordering.api.models.repositories.CartRepository
+import com.matb.ordering.api.models.requests.CartStateUpdatingRequest
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api")
+class ChefController (
+        private val cartRepository: CartRepository
+){
+    @GetMapping("/vendor/{id}/chef")
+    fun getAllPendingCart(){
+
+    }
+
+    @PostMapping("/vendor/{id}/chef")
+    fun updateCartState(@RequestBody cartStateUpdatingRequest: CartStateUpdatingRequest){
+
+    }
 }

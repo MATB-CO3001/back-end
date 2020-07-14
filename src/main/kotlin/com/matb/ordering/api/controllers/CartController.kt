336836c1790368a6392a822e9f46fee0a5478d38
nganvn <nganvn2110@gmail.com>
2020-07-14 15:14:08 +0700

    add god-feature

--- src/main/kotlin/com/matb/ordering/api/controllers/CartController.kt
+++ src/main/kotlin/com/matb/ordering/api/controllers/CartController.kt
@@ -6,15 +6,14 @@ import com.matb.ordering.api.models.entities.Cart
 import com.matb.ordering.api.models.entities.CartItem
 import com.matb.ordering.api.models.repositories.*
 import com.matb.ordering.api.models.requests.creation.CartRequest
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.web.bind.annotation.*
+import java.lang.Math.abs
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.util.*
 
 @RestController
 @RequestMapping("/api/cart")
@@ -60,6 +59,25 @@ class CartController(
             throw UserNotFoundException("customer", username)
         }
         return ResponseEntity(cartRepository.findAllByCustomer(username), HttpStatus.OK)
+    }
+
+    @PostMapping("/cheat")
+    fun cheatCart(@RequestBody cartRequest: CartRequest): ResponseEntity<Cart> {
+        var cart = Cart(
+                0,
+                cartRequest.customer,
+                cartRequest.vendor,
+                CartState.PENDING,
+                LocalDateTime.now(ZoneId.of("GMT+07:00")).plusDays(-abs(Random().nextLong()%30))
+        )
+        var temporarySavedCart = cartRepository.save(cart)
+        for (item in cartRequest.orderedFoodList) {
+            var food = foodRepository.findById(item.foodId).get()
+            temporarySavedCart.total += food.price * item.quantity
+            cartItemRepository.save(CartItem(food,cart,item.quantity))
+        }
+        template.convertAndSend("/chef-message", "")
+        return ResponseEntity(cartRepository.save(temporarySavedCart),HttpStatus.CREATED)
     }
 
 }
